cff47724bc41dda81a02ac249c26146f93bed9ed
gabypega1997 <gabypega1997@gmail.com>
2022-06-06 14:09:22 +0200

    add font-family

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,14 @@ const IndexPage = () => (
   <Layout title="Gabriella's Photography">
     <div>
       <img src="/img/Background.png" alt="Background" className='absolute  top-0 left-0 -z-50 h-screen w-screen'  />
-      <div className='w-3/5 m-auto '>
-        <h1 className='text-white mt-10'>
+      <div className='w-3/5 m-auto font-fancy text-xl'>
+        <h1 className='text-white mt-10 mb-32'>
           “ Photography, an <br/>
           unlimited universe, <br/>
           in a limited world <br/>
           of fate. <br/>
         </h1>
-        <p className='m-10 text-center p-2 rounded-3xl bg-[#FF00FF]'>
+        <p className='m-10 text-center p-2 w-40 rounded-3xl bg-button-purple '>
           <Link href="/about">
             <a className='text-white'>See Photos</a>
           </Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,15 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        'navbar-gray': 'rgba(0, 0, 0, 0.55)'
+        'navbar-gray': 'rgba(0, 0, 0, 0.55)',
+        'button-purple': '#FF00FF99'
+
       },
       backgroundImage:{
         "home-background": "url('/img/Background.png')"
+      },
+      fontFamily: {
+        fancy: ['Dancing Script', 'sans-serif']
       }
     },
   },
