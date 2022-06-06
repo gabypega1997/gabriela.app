8bee776f3bf7e151aab5585fd6b51a5c6aed7927
gabypega1997 <gabypega1997@gmail.com>
2022-06-06 15:31:29 +0200

    galery 20%

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,13 +3,28 @@ import Layout from '../components/Layout'
 
 const AboutPage = () => (
   <Layout title="Gabriella's Gallery">
-    <h1>About</h1>
-    <p>This is the about page</p>
-    <p>
-      <Link href="/">
-        <a>Go home</a>
-      </Link>
-    </p>
+    <div className='bg-gray-600 absolute h-16 w-screen top-0 left-0 -z-50'>
+    </div>
+    <div className='flex-col gap-3 pt-5 bg-gray-600'>
+        <div className="h-56 w-screen flex justify-center">
+          <img src='/img/Background.png' className='w-4/5 h-4/5' ></img>
+        </div>
+        <div className="h-56  flex justify-center ">
+        <img src='/img/Background.png' className='w-4/5 h-4/5' ></img>
+        </div>
+        <div className="h-56   w-screen flex justify-center">
+        <img src='/img/Background.png' className='w-4/5 h-4/5' ></img>
+        </div>
+        <div className="h-56   w-screen flex justify-center">
+        <img src='/img/Background.png' className='w-4/5 h-4/5' ></img>
+        </div>
+        <div className="h-56   w-screen flex justify-center">
+        <img src='/img/Background.png' className='w-4/5 h-4/5' ></img>
+        </div>
+
+    </div>
+
+    
   </Layout>
 )
 
