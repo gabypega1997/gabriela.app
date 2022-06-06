112448e9f36d01011d95404f87962998a9bd8759
gabypega1997 <gabypega1997@gmail.com>
2022-06-06 15:05:39 +0200

    home page Done

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -16,23 +16,23 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     <header>
-      <nav className='bg-navbar-gray h-16'>
+      <nav className='bg-navbar-gray h-16 md:h-20'>
         <div className='w-4/5 m-auto h-full flex justify-between'>
           <div className=''>
             <Link href="/" >
-              <img src="/img/Logo.png" alt="Logo" width={100}  className='pt-4' />
+              <img src="/img/Logo.png" alt="Logo" width={100}  className='pt-4 md:w-32 lg:w-40' />
             </Link>{' '}
 
           </div>
-          <div className='flex pt-5 gap-3'>
+          <div className='flex pt-5 gap-3 md:gap-6 lg:gap-10 '>
               <a href="mailto:gabrielagianina31@gmail.com" >
-                <img src='/img/MailLogo.png' alt='mail logo' width={25} ></img>
+                <img src='/img/MailLogo.png' alt='mail logo' width={25} className="hover:scale-90 md:w-10"></img>
               </a>
-              <a href="https://www.facebook.com/Gabriela.Gianina31" target="_blank">
-                <img src='/img/FacebookLogo.png' alt='facebook logo' width={15}></img>
+              <a href="https://www.facebook.com/Gabriela.Gianina31" target="_blank" >
+                <img src='/img/FacebookLogo.png' alt='facebook logo' width={15} className="hover:scale-90 md:w-6"></img>
               </a>
               <a href="http://instagram.com/gabriella.gianina31" target="_blank">
-                <img src='/img/InstaLogo.png' alt=' instagram logo' width={20}></img>
+                <img src='/img/InstaLogo.png' alt=' instagram logo' width={20} className="hover:scale-90 md:w-8"></img>
               </a>
           </div>
           
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,16 +5,16 @@ const IndexPage = () => (
   <Layout title="Gabriella's Photography">
     <div>
       <img src="/img/Background.png" alt="Background" className='absolute  top-0 left-0 -z-50 h-screen w-screen'  />
-      <div className='w-3/5 m-auto font-fancy text-xl'>
-        <h1 className='text-fancy-white mt-10 mb-32'>
+      <div className='w-3/5 m-auto font-fancy text-xl md:text-2xl'>
+        <h1 className='text-fancy-white mt-10 mb-32 md:text-4xl xl:mt-32 md:h-96 xl:text-6xl'>
           “ Photography, an <br/>
           unlimited universe, <br/>
           in a limited world <br/>
           of fate. <br/>
         </h1>
-        <p className='m-10 text-center p-2 w-40 rounded-3xl bg-button-purple '>
+        <p className='m-auto text-center p-2 w-40 rounded-3xl bg-button-purple hover:scale-95 md:ml-96 lg:ml-auto lg:w-80'>
           <Link href="/about">
-            <a className='text-fancy-white'>See Photos</a>
+            <a className='text-fancy-white '>See Photos</a>
           </Link>
         </p>
 
