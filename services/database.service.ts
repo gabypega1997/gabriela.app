9b94ad78e161a25677b0f562952432969fa7dbd9
gabypega1997 <gabypega1997@gmail.com>
2022-06-12 20:58:36 +0200

    setup mongodb database.service.ts 100%

--- services/database.service.ts
+++ services/database.service.ts
@@ -5,7 +5,23 @@ import * as dotenv from "dotenv";
 
 // Global Variables
 
-export const collections: { p?: mongoDB.Collection } = {}
+export const collections: { photos?: mongoDB.Collection } = {}
 
 
 // Initialize Connection
+
+export async function connectToDatabase () {
+    dotenv.config();
+    
+        const client: mongoDB.MongoClient = new mongoDB.MongoClient(process.env.DB_CONN_STRING);
+                
+        await client.connect();
+            
+        const db: mongoDB.Db = client.db(process.env.DB_NAME);
+    
+        const photosCollection: mongoDB.Collection = db.collection(process.env.PHOTO_COLLECTION_NAME);
+    
+    collections.photos = photosCollection;
+        
+            console.log(`Successfully connected to database: ${db.databaseName} and collection: ${photosCollection.collectionName}`);
+    }
