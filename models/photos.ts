d72c1cb7dc273a8db9cc9503fc53f05f3a2cd06e
gabypega1997 <gabypega1997@gmail.com>
2022-06-12 20:45:57 +0200

    setup mongodb database.service.ts 50%

--- models/photos.ts
+++ models/photos.ts
@@ -1,2 +1,5 @@
 import { ObjectId } from "mongodb";
 
+export default class Photo {
+    constructor (public image: string, public location: string, public date: number, public id?: ObjectId ) {}
+}
---
+++ services/database.service.ts
@@ -0,0 +1,11 @@
+// External Dependencies
+
+import * as mongoDB from "mongodb";
+import * as dotenv from "dotenv";
+
+// Global Variables
+
+export const collections: { p?: mongoDB.Collection } = {}
+
+
+// Initialize Connection
